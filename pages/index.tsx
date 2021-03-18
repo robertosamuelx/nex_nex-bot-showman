99d8d8d553fb9b216bfd0ed85460486fd408f6ee
robertosamuelx <roberto-samuel1@hotmail.com>
2021-03-18 02:39:24 -0300

    add category and salesman name

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,8 @@ import { IoPersonSharp } from 'react-icons/io5'
 import { VscLoading, VscFilter } from 'react-icons/vsc'
 import { useToasts } from 'react-toast-notifications'
 
-const controlIcons = {
-  status: ['ON BOT', 'WAITING', 'ON ATTENDANT'],
-  icons: [<FaRobot />, <VscLoading />, <IoPersonSharp />]
-}
-
 interface Chat {
+  id: Number,
   user: String,
   name?: String,
   status: String,
@@ -38,7 +34,13 @@ export default function Home() {
   const [latest, setLatest] = useState<Chat[]>([])
   const [contacts, setContacts] = useState([])
   const [botCache, setBotCache] = useState([])
+  const [sales, setSales] = useState([])
   const { addToast } = useToasts()
+  const [editName, setEditName] = useState(true)
+  const [buttonText, setButtonText] = useState('Editar Nome')
+  const [sallesmanName, setSallesmanName] = useState('...')
+  const [showInputNameSallesman, setShowInputNameSallesman] = useState('none')
+  const [showMenu, setShowMenu] = useState(false)
 
   async function getContacts() {
     const res = await fetch('http://localhost:5000/contacts')
@@ -54,9 +56,17 @@ export default function Home() {
     return json
   }
 
+  async function getSales() {
+    const res = await fetch('http://localhost:3001/salesman')
+    const json = await res.json()
+    setSales(json)
+    return json
+  }
+
   async function getChats() {
     const contacts_ = await getContacts()
     const cached = await getBotCache()
+    const sales = await getSales()
     fetch('http://localhost:3001/chats')
       .then(res => res.json())
       .then(json => {
@@ -132,7 +142,11 @@ export default function Home() {
         return message
     })
     return (
-      <div className="item" onClick={e => {
+      <div className="item" style={
+        {
+          backgroundColor: chat.status == 'WAITING' ? '#98FB98' : chat.status == 'ON BOT' ? '#B0C4DE' : '#fff'
+        }
+      } onClick={e => {
         setActive({ number: chat.user, name: chat.name })
       }}>
         <div className="title is-6">
@@ -140,7 +154,12 @@ export default function Home() {
         </div>
         <div className="subtitle is-6">
           {formattedMessages[formattedMessages.length - 1].value}
-          {controlIcons.icons[controlIcons.status.indexOf(chat.status.toString())]}
+          {sales.map( sale => {
+            if(sale.chatId === chat.id)
+              return <b>{sale.salesmanName}</b>
+              }
+            )
+          }
         </div>
       </div>
     )
@@ -159,17 +178,47 @@ export default function Home() {
     )
   }
 
+
   return (<div>
+    <div className="container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <input type="text" className="input" value={sallesmanName} onChange={e => {
+          setSallesmanName(e.target.value)
+          console.log(e.target.value)
+        }} style={{ width: '50%', display: showInputNameSallesman }} readOnly={editName} />
+        <h3 className="title" style={{ display: showInputNameSallesman === 'block' ? 'none' : 'block' }}>Bem-vindo(a) {sallesmanName}</h3>
+        <button className="button is-link" onClick={() => {
+          setEditName(!editName)
+          if (buttonText === 'Salvar') {
+            addToast('Nome do vendedor definido para '.concat(sallesmanName), { appearance: 'success', autoDismiss: true })
+            setShowInputNameSallesman('none')
+          }
+          else
+            setShowInputNameSallesman('block')
+          setButtonText(buttonText === 'Salvar' ? 'Editar Nome' : 'Salvar')
+        }}>{buttonText}</button>
+      </div>
     <section className="section">
       <div className="container">
         <div className="columns">
           <div className="column is-3 lista-de-conversas">
             <div className="barra-superior">
               <span>
-                Categorizar por: <VscFilter />
-              </span>
+                Categorizar por: <button className="button is-info" onClick={() => {
+                  setShowMenu(!showMenu)
+                }}>
+                  <VscFilter />
+                </button>
+              {showMenu && 
+                <div className="my-menu">
+                  <ul>
+                  <li><input type="checkbox" /> Não lidas</li>
+                  <li><input type="checkbox" /> Vendedor</li>
+                  <li><input type="checkbox" /> Pergunta</li>
+                  </ul>
+                </div>}
+                </span>
             </div>
-            {latest.map(i => <ItemChat key={i.user.toString()} chat={i} />)}
+            {latest.map(i => <ItemChat key={i.id.valueOf()} chat={i} />)}
           </div>
           <div className="column conversa-ativa">
             <div className="barra-superior">
