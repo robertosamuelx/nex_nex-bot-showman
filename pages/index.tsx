35033941baee8c82961dea760e33b6275deae0f7
robertosamuelx <roberto-samuel1@hotmail.com>
2021-03-23 00:07:57 -0300

    lista de categorias and more...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
 import moment from 'moment'
-import { FaRobot } from 'react-icons/fa'
-import { IoPersonSharp } from 'react-icons/io5'
-import { VscLoading, VscFilter } from 'react-icons/vsc'
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa'
 import { useToasts } from 'react-toast-notifications'
 
 interface Chat {
@@ -27,6 +23,16 @@ interface Message {
   isFromMe: Boolean
 }
 
+interface Categorie {
+  value: String,
+  options: Array<String>
+}
+
+interface Filter {
+  field: String[],
+  value: String[]
+}
+
 export default function Home() {
 
   const [active, setActive] = useState<Active>({ number: '', name: '' })
@@ -40,7 +46,15 @@ export default function Home() {
   const [buttonText, setButtonText] = useState('Editar Nome')
   const [sallesmanName, setSallesmanName] = useState('...')
   const [showInputNameSallesman, setShowInputNameSallesman] = useState('none')
-  const [showMenu, setShowMenu] = useState(false)
+  const [categories, setCategories] = useState<Categorie[]>([])
+  const [mustShowOptions, setMustShowOptions] = useState([])
+  const [filter, setFilter] = useState<Filter>({ field: [], value: [] })
+
+  async function getCategories() {
+    const res = await fetch('http://localhost:3001/categories')
+    const json = await res.json()
+    setCategories(json)
+  }
 
   async function getContacts() {
     const res = await fetch('http://localhost:5000/contacts')
@@ -57,20 +71,31 @@ export default function Home() {
   }
 
   async function getSales() {
-    const res = await fetch('http://localhost:3001/salesman')
+    const res = await fetch('http://localhost:3001/sales')
     const json = await res.json()
     setSales(json)
     return json
   }
 
   async function getChats() {
+    await getCategories()
     const contacts_ = await getContacts()
     const cached = await getBotCache()
     const sales = await getSales()
-    fetch('http://localhost:3001/chats')
+    fetch('http://localhost:3001/chats',
+      {
+        body: JSON.stringify(filter),
+        method: 'POST'
+      }
+    )
       .then(res => res.json())
       .then(json => {
         const result = json.map(el => {
+
+          if (filter.field.includes('vendedor')) {
+
+          }
+
           let name = ''
           let status = 'ON ATTENDANT'
 
@@ -154,11 +179,11 @@ export default function Home() {
         </div>
         <div className="subtitle is-6">
           {formattedMessages[formattedMessages.length - 1].value}
-          {sales.map( sale => {
-            if(sale.chatId === chat.id)
+          {sales.map(sale => {
+            if (sale.chatId === chat.id)
               return <b>{sale.salesmanName}</b>
-              }
-            )
+          }
+          )
           }
         </div>
       </div>
@@ -178,9 +203,49 @@ export default function Home() {
     )
   }
 
+  function OptionItemCategorie(props: { categorie: Categorie }) {
+    const { categorie } = props
+    return (
+      <ul>
+        {mustShowOptions.includes(categorie.value) ?
+          categorie.options.map(option => <li className="categorie-item-option" onClick={() => {
+
+          }}>{option}</li>) :
+          <div />}
+      </ul>
+    )
+  }
+
+  function ItemCategorie(props: { categorie: Categorie }) {
+    const { categorie } = props
+    return (
+      <div
+        className="categorie"
+        onClick={() => {
+          if (!mustShowOptions.includes(categorie.value))
+            mustShowOptions.push(categorie.value)
+          else
+            mustShowOptions.splice(mustShowOptions.indexOf(categorie.value), 1)
+          setMustShowOptions(mustShowOptions)
+        }}>
+        <div className="categorie-item">
+          {categorie.value}
+
+          {categorie.options.length > 0 &&
+            <div>
+              <FaArrowDown style={{ display: mustShowOptions.includes(categorie.value) ? "none" : "block" }} />
+              <FaArrowUp style={{ display: mustShowOptions.includes(categorie.value) ? "block" : "none" }} />
+            </div>
+          }
+        </div>
+        <OptionItemCategorie categorie={categorie} />
+      </div>
+    )
+  }
+
 
   return (<div>
-    <div className="container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <div className="container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <input type="text" className="input" value={sallesmanName} onChange={e => {
           setSallesmanName(e.target.value)
           console.log(e.target.value)
@@ -197,59 +262,52 @@ export default function Home() {
           setButtonText(buttonText === 'Salvar' ? 'Editar Nome' : 'Salvar')
         }}>{buttonText}</button>
       </div>
-    <section className="section">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-3 lista-de-conversas">
-            <div className="barra-superior">
-              <span>
-                Categorizar por: <button className="button is-info" onClick={() => {
-                  setShowMenu(!showMenu)
-                }}>
-                  <VscFilter />
-                </button>
-              {showMenu && 
-                <div className="my-menu">
-                  <ul>
-                  <li><input type="checkbox" /> Não lidas</li>
-                  <li><input type="checkbox" /> Vendedor</li>
-                  <li><input type="checkbox" /> Pergunta</li>
-                  </ul>
-                </div>}
-                </span>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-2 lista-de-categorias">
+              <div className="barra-superior">
+                <span>Categorias</span>
+              </div>
+              <div className="categories">
+                {categories.map(categorie => <ItemCategorie categorie={categorie} key={categorie.value.toString()} />)}
+              </div>
             </div>
-            {latest.map(i => <ItemChat key={i.id.valueOf()} chat={i} />)}
-          </div>
-          <div className="column conversa-ativa">
-            <div className="barra-superior">
-              <span>{active.name}</span>
+            <div className="column is-3 lista-de-conversas">
+              <div className="barra-superior">
+                <span>Conversas</span>
+              </div>
+              {latest.map(i => <ItemChat key={i.id.valueOf()} chat={i} />)}
             </div>
-            <div className="lista-mensagens">
-              <ul className="lista-mensagens-ul" >
-                {latest.map(chat => {
-                  if (active.number === chat.user)
-                    return chat.messages.map(message =>
-                      <li>
-                        <ItemMessage
-                          message={message} />
-                      </li>
-                    )
-                })}
-              </ul>
-            </div>
-            <div className="barra-inferior">
-              <input type="text" className="input" placeholder="Insira a mensagem" onChange={e => {
-                setMyMessage(e.target.value)
-              }}
-                onKeyPress={e => {
-                  sendMessage(e, active.number, myMessage)
+            <div className="column conversa-ativa">
+              <div className="barra-superior">
+                <span>{active.name}</span>
+              </div>
+              <div className="lista-mensagens">
+                <ul className="lista-mensagens-ul" >
+                  {latest.map(chat => {
+                    if (active.number === chat.user)
+                      return chat.messages.map(message =>
+                        <li>
+                          <ItemMessage
+                            message={message} />
+                        </li>
+                      )
+                  })}
+                </ul>
+              </div>
+              <div className="barra-inferior">
+                <input type="text" className="input" placeholder="Insira a mensagem" onChange={e => {
+                  setMyMessage(e.target.value)
                 }}
-                value={myMessage} />
+                  onKeyPress={e => {
+                    sendMessage(e, active.number, myMessage)
+                  }}
+                  value={myMessage} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </div>
-  )
+      </section>
+    </div>)
 }
