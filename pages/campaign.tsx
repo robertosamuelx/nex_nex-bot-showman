55490f9d4fb6f7c4b647dbf75a0ceb63573fce12
robertosamuelx <roberto-samuel1@hotmail.com>
2021-06-10 00:39:01 -0300

    redesign campaign page

--- pages/campaign.tsx
+++ pages/campaign.tsx
@@ -1,67 +1,98 @@
 import { GetServerSideProps } from "next"
 import { signOut, getSession } from 'next-auth/client'
 import { useState, useEffect, useRef } from 'react'
-import { BsSearch } from 'react-icons/bs'
+import moment from 'moment'
 
 interface Contact {
     name: string,
     number: string
 }
 
+interface Campaign {
+    id: string,
+    name: string,
+    contacts: Contact[],
+    createdAt: Date
+}
+
 export default function Campaign({ endpoint, session }) {
     const [contacts, setContacts] = useState<Contact[]>([])
     const [searchedContact, setSearchedContact] = useState("")
-    const scrollRef = useRef(null)
+    const [campaigns, setCampaigns] = useState<Campaign[]>([])
+    const [selectedCampaign, setSelectedCampaign] = useState<Campaign>({ id: "", name: "", contacts: [], createdAt: null })
 
-    const scrollToBottom = () => {
-        scrollRef.current?.scrollIntoView({ behavior: "smooth" })
-      }
-
-    async function getContacts(){
+    async function getContacts() {
         const res = await fetch(endpoint + '/contacts')
         const json = await res.json()
         setContacts(json)
     }
 
-    useEffect(() => {
-        getContacts()
-    },[])
+    async function getCampaigns() {
+        const res = await fetch(endpoint + '/campaigns')
+        const json = await res.json()
+        setCampaigns(json)
+    }
 
-    function ItemContact(props: { contact:Contact, index: number }){
-        return (<p ref={scrollRef}>{props.contact.name}</p>)
+    useEffect(() => {
+        //getContacts()
+        getCampaigns()
+    }, [])
+
+    function ItemCampaign(props: { campaign: Campaign, index: number }) {
+        return (<option value={props.index}>{props.campaign.name}</option>)
     }
 
     return (
         <section className="section">
             <div className="container">
                 <div className="columns">
-                    <div className="column">
+                    <div className="column is-4">
                         <div className="card">
                             <div className="card-header">
                                 <h3 className="card-header-title title is-5">
-                                    Contatos
+                                    Campanhas
                                 </h3>
                             </div>
-                            <div className="card-content">
-                                <div style={{display: "flex", justifyContent: "space-around", margin: "5px", alignItems: "center"}}>
-                                    <input className="input" value={searchedContact} onChange={e => {setSearchedContact(e.target.value)}}/>
-                                    <BsSearch size={25} onClick={() => {
-                                        setContacts(contacts.filter(contact => contact.name.toLowerCase().includes(searchedContact.toLocaleLowerCase())))
-                                    }}/>
+                            <div className="card-content" style={{ display: "flex", justifyContent: "space-between" }}>
+                                <div className="select is-info">
+                                    <select onChange={e => {
+                                        setSelectedCampaign(campaigns[e.target.value])
+                                    }}>
+                                        <option value={0}>Escolha</option>
+                                        {campaigns.map((campaign, index) => <ItemCampaign campaign={campaign} key={campaign.id} index={index} />)}
+                                    </select>
                                 </div>
-                                <div className="content" style={{maxHeight: "500px", overflowX: "hidden", overflowY: "scroll"}}>
-                                    {contacts.map((contact, index) => <ItemContact contact={contact} key={contact.number} index={index}/>)}
-                                </div>
+                                <button
+                                    className="button is-info"
+                                    onClick={() => { }}>Criar nova</button>
                             </div>
                         </div>
                     </div>
                     <div className="column">
-                        <button
-                            className="button is-link"
-                            style={{ margin: "5px" }}
-                            onClick={() => {
-                                signOut({ callbackUrl: '/' })
-                            }}>Sair</button>
+                        <div className="card">
+                            <div className="card-header">
+                                <h3 className="card-header-title title is-5">
+                                    Detalhes
+                </h3>
+                                <button
+                                    className="button is-link"
+                                    style={{ margin: "5px" }}
+                                    onClick={() => {
+                                        signOut({ callbackUrl: '/' })
+                                    }}>Sair</button>
+                            </div>
+                            {selectedCampaign.id !== "" &&
+                                <div className="card-content">
+                                    <div className="content">
+                                        <p>Nome: {selectedCampaign.name}</p>
+                                        <p>Data de disparo: {selectedCampaign.createdAt ? moment(selectedCampaign.createdAt.toString()).format("DD/MM/YYYY") : ""}</p>
+                                        <p>Contatos impactados: <strong>{selectedCampaign.contacts.length}</strong></p>
+                                        <ul style={{listStyle: "none"}}>
+                                            {selectedCampaign.contacts.map(contact => <li key={contact.number}>{contact.name}</li>)}
+                                        </ul>
+                                    </div>
+                                </div>}
+                        </div>
                     </div>
                 </div>
             </div>
