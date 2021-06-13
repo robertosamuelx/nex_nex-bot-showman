d6704e31da1ad5e972ec9adaa97fceed3470efbe
robertosamuelx <roberto-samuel1@hotmail.com>
2021-06-13 01:40:44 -0300

    add create campaign

--- pages/campaign.tsx
+++ pages/campaign.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next"
 import { signOut, getSession } from 'next-auth/client'
 import { useState, useEffect, useRef } from 'react'
 import moment from 'moment'
+import { FaUpload } from 'react-icons/fa'
 
 interface Contact {
     name: string,
@@ -20,6 +21,8 @@ export default function Campaign({ endpoint, session }) {
     const [searchedContact, setSearchedContact] = useState("")
     const [campaigns, setCampaigns] = useState<Campaign[]>([])
     const [selectedCampaign, setSelectedCampaign] = useState<Campaign>({ id: "", name: "", contacts: [], createdAt: null })
+    const [newCampaign, setNewCampaign] = useState<Campaign>({ id: "", name: "", contacts: [], createdAt: null })
+    const [show, setShow] = useState("")
 
     async function getContacts() {
         const res = await fetch(endpoint + '/contacts')
@@ -57,6 +60,7 @@ export default function Campaign({ endpoint, session }) {
                                 <div className="select is-info">
                                     <select onChange={e => {
                                         setSelectedCampaign(campaigns[e.target.value])
+                                        setShow("camp_details")
                                     }}>
                                         <option value={0}>Escolha</option>
                                         {campaigns.map((campaign, index) => <ItemCampaign campaign={campaign} key={campaign.id} index={index} />)}
@@ -64,7 +68,9 @@ export default function Campaign({ endpoint, session }) {
                                 </div>
                                 <button
                                     className="button is-info"
-                                    onClick={() => { }}>Criar nova</button>
+                                    onClick={() => {
+                                        setShow("camp_create")
+                                    }}>Criar nova</button>
                             </div>
                         </div>
                     </div>
@@ -73,7 +79,7 @@ export default function Campaign({ endpoint, session }) {
                             <div className="card-header">
                                 <h3 className="card-header-title title is-5">
                                     Detalhes
-                </h3>
+                                </h3>
                                 <button
                                     className="button is-link"
                                     style={{ margin: "5px" }}
@@ -81,15 +87,52 @@ export default function Campaign({ endpoint, session }) {
                                         signOut({ callbackUrl: '/' })
                                     }}>Sair</button>
                             </div>
-                            {selectedCampaign.id !== "" &&
+                            {show == "camp_details" &&
                                 <div className="card-content">
                                     <div className="content">
                                         <p>Nome: {selectedCampaign.name}</p>
                                         <p>Data de disparo: {selectedCampaign.createdAt ? moment(selectedCampaign.createdAt.toString()).format("DD/MM/YYYY") : ""}</p>
                                         <p>Contatos impactados: <strong>{selectedCampaign.contacts.length}</strong></p>
-                                        <ul style={{listStyle: "none"}}>
+                                        <ul style={{ listStyle: "none" }}>
                                             {selectedCampaign.contacts.map(contact => <li key={contact.number}>{contact.name}</li>)}
                                         </ul>
+                                    </div>
+                                </div>}
+                            {show == "camp_create" &&
+                                <div className="card-content">
+                                    <div className="content">
+                                        <div className="field">
+                                            <label className="label">Nome</label>
+                                            <div className="control">
+                                                <input
+                                                    className="input"
+                                                    type="input"
+                                                    placeholder="Promoção do próximo sábado!"
+                                                    value={newCampaign.name.toString()}
+                                                    onChange={e => {
+                                                        setNewCampaign({ ...newCampaign, name: e.target.value })
+                                                    }} />
+                                            </div>
+                                        </div>
+                                        <div className="field">
+                                            <label className="label">Lista de Contatos</label>
+                                            <div className="control">
+                                                <div className="file">
+                                                    <label className="file-label">
+                                                        <input className="file-input" type="file" name="base" />
+                                                        <span className="file-cta">
+                                                            <span className="file-icon">
+                                                                <FaUpload />
+                                                            </span>
+                                                            <span className="file-label">Escolha um arquivo...</span>
+                                                        </span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div className="control">
+                                                
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>}
                         </div>
